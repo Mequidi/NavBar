e0a3d75ee97a9190113d78c5e21d084d78f4c8f6
Mequidi <nikhilsejwal234@gmail.com>
2021-06-09 15:26:17 +0530

    Gotta set navbar height

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ const toggleBtn = document.querySelector(".toggle-btn");
 const linkList = document.querySelector(".link-list");
 const navbar = document.querySelector(".navbar")
 
+let temp;
+
 toggleBtn.addEventListener("click",function(){
-    console.log(linkList.classList.contains("show-list"))
-    if(linkList.classList.contains("show-list")){
+    if(linkList.classList.contains("show-list"))
+    {
         linkList.classList.remove("show-list");
-        console.log(linkList.classList.contains("show-list"))
     }
     else    
     {
         linkList.classList.add("show-list");
-        console.log(linkList.classList.contains("show-list"))
     }
 })
